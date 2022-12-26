dff20aa606849bff87fcc8a7a9376dc4578bdc1b
ReganLaw <609607745@qq.com>
2022-12-26 18:57:14 +0800

    idea

--- Java/workA1.java
+++ Java/workA1.java
@@ -5,24 +5,13 @@ public class workA1 {
         Scanner sc=new Scanner(System.in);
         System.out.println("请输入学生成绩：");
         int score=sc.nextInt();
-        switch(score/10) {
-            case(9):
-                System.out.println("等级A");
-                break;
-            case(8):
-                System.out.println("等级B");
-                break;
-            case(7):
-                System.out.println("等级C");
-                break;
-            case(6):
-                System.out.println("等级D");
-                break;
-            default:
-                System.out.println("等级E");
-                break;
+        switch(score/10){
+            case (9) -> System.out.println("等级A");
+            case (8) -> System.out.println("等级B");
+            case (7) -> System.out.println("等级C");
+            case (6) -> System.out.println("等级D");
+            default -> System.out.println("等级E");
         }
-;
         sc.close();
     }
 }
