a60149566c6ff305a8666027db95f1d68b48b3a2
ReganLaw <reganlaw777@outlook.com>
2023-10-24 15:42:37 +0800

    Experiment2

--- JavaDesignPatterns/src/main/java/Experiment2/EngineAdapter/Car.java
+++ JavaDesignPatterns/src/main/java/Experiment2/EngineAdapter/Car.java
@@ -1,10 +1,28 @@
 package Experiment2.EngineAdapter;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class Car{
     private EngineAdapter engineAdapter;
 
-    public Car(EngineAdapter engineAdapter){
-        this.engineAdapter=engineAdapter;
+    public Car(String configFile){
+        try{
+            DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder=factory.newDocumentBuilder();
+            Document document=builder.parse(new File(configFile));
+
+            String adapterClass=document.getElementsByTagName("class").item(0).getTextContent();
+            String engineClass=document.getElementsByTagName("class").item(1).getTextContent();
+
+            Engine engine=(Engine)Class.forName(engineClass).getDeclaredConstructor().newInstance();
+            engineAdapter=(EngineAdapter)Class.forName(adapterClass).getDeclaredConstructor(Engine.class).newInstance(engine);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void drive(){
--- JavaDesignPatterns/src/main/java/Experiment2/EngineAdapter/client.java
+++ JavaDesignPatterns/src/main/java/Experiment2/EngineAdapter/client.java
@@ -2,14 +2,7 @@ package Experiment2.EngineAdapter;
 
 public class client{
     public static void main(String[] args){
-        Engine electricEngine=new ElectricEngine();
-        EngineAdapter electricEngineAdapter=new EngineAdapter(electricEngine);
-        Car electricCar=new Car(electricEngineAdapter);
-        electricCar.drive();
-
-        Engine solarEngine=new SolarEngine();
-        EngineAdapter solarEngineAdapter=new EngineAdapter(solarEngine);
-        Car solarCar=new Car(solarEngineAdapter);
-        solarCar.drive();
+        Car car=new Car("./src/main/java/Experiment2/EngineAdapter/adapter-config.xml");
+        car.drive();
     }
 }
